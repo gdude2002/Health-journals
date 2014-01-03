86bd0b4deaad4f69d4266f28edee314c4c121ec4
Gareth <gdude2002@gmail.com>
2014-01-03 17:23:11 +0000

    Code and stuff

--- core/routes.py
+++ core/routes.py
@@ -1,1 +1,73 @@
 __author__ = 'Gareth Coles'
+
+GET = "GET"
+POST = "POST"
+
+from bottle import HTTPError, request
+from bottle import mako_template as template
+from bottle import mako_view as view
+
+
+class Routes(object):
+
+    app = None
+    cork = None
+
+    def __init__(self, app, cork):
+        self.app = app
+        self.cork = cork
+        app.route("/", [GET, POST], self.index)
+        app.route("/api", [GET, POST], self.api_root)
+        app.route("/api/", [GET, POST], self.api_root)
+        app.route("/login", GET, self.login)
+        app.route("/login", POST, self.login_post)
+        app.route("/login/", GET, self.login)
+        app.route("/login/", POST, self.login_post)
+        app.route("/register", GET, self.register)
+        app.route("/register", POST, self.register_post)
+        app.route("/register/", GET, self.register)
+        app.route("/register/", POST, self.register_post)
+
+    @view("index")
+    def index(self):
+        return "Hello, world!"
+
+    def register(self):
+        pass
+
+    def register_post(self):
+        username = request.POST.get('user', '')
+        password = request.POST.get('pwd', '')
+        if username and password:
+            self.cork.register(username, password, ' ')
+        else:
+            if username:
+                pass  # No password
+            else:
+                pass  # No username
+
+    def login(self):
+        pass
+
+    def login_post(self):
+        username = request.POST.get('user', '')
+        password = request.POST.get('pwd', '')
+        if username and password:
+            self.cork.login(username, password,
+                            success_redirect='/?message=logged_in',
+                            fail_redirect='/login?error=wrong')
+        else:
+            if username:
+                return HTTPError(300, "/login?error=password")
+            else:
+                return HTTPError(300, "/login?error=username")
+
+    def api_login(self):
+        username = request.POST.get('user', '')
+        password = request.POST.get('pwd', '')
+        self.cork.login(username, password,
+                        success_redirect='/',
+                        fail_redirect='/login')
+
+    def api_root(self):
+        raise HTTPError(403, "GTFO.")
--- run.py
+++ run.py
@@ -1,18 +1,27 @@
+
 __author__ = 'Gareth Coles'
 
 import logging
 import sys
 import yaml
-from bottle import run, default_app, hook, request
+
+import bottle
+
+from beaker.middleware import SessionMiddleware
+from cork import Cork
+from cork.sqlalchemy_backend import SqlAlchemyBackend
 
 from core.sql import Setup
+from core.routes import Routes
+
+bottle.TEMPLATE_PATH.append("templates")
 
 logging.basicConfig(
     format="%(asctime)s | %(levelname)8s | %(message)s",
     datefmt="%d %b %Y - %H:%M:%S",
     level=(logging.DEBUG if "--debug" in sys.argv else logging.INFO))
 
-logger = logging.getLogger("Main")
+logger = logging.getLogger()
 
 fh = open("config/config.yml", "r")
 config = yaml.load(fh)
@@ -23,17 +32,34 @@ db_url = "%s://%s:%s@%s/%s"\
          % (db["type"], db["username"], db["password"], db["address"],
             db["database"])
 
-app = default_app()
+session_opts = {
+    'cache.type': 'file',
+    'cache.data_dir': 'cache/data',
+    'cache.lock_dir': 'cache/lock',
+    'session.type': 'ext:database',
+    'session.url': db_url,
+    'session.validate_key': True,
+}
 
-
+app = bottle.default_app()
 sql_setup = Setup(app, db_url)
 
+backend = SqlAlchemyBackend(db_url, "users", "roles", "pending_users")
+cork = Cork(backend=backend, initialize=True)
 
-@hook('before_request')
+routes_setup = Routes(app, cork)
+
+
+@bottle.hook('before_request')
 def log_all():
-    ip = request.remote_addr
-    logger.info("[%s] %s %s" % (ip, request.method, request.fullpath))
+    ip = bottle.request.remote_addr
+    logger.info("[%s] %s %s" % (ip, bottle.request.method,
+                                bottle.request.fullpath))
+
+app = SessionMiddleware(app, session_opts)
+
+application = app  # For uWSGI
 
 if __name__ == "__main__":
-    run(app, server="cherrypy", host=config["server"]["host"],
-        port=config["server"]["port"])
+    bottle.run(app, server="cherrypy", host=config["server"]["host"],
+               port=config["server"]["port"])
